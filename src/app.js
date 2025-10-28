1f57f123fc8c4ebe0cb24bab54154dbba826358f
Luz Torres <luxztorres@gmail.com>
2025-10-28 00:09:40 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+const app = express();
+const PORT = process.env.PORT||8080;
+const CONNECTION = mongoose.connect(`mongodb+srv://lxz:adminx2@cluster0.x8smpie.mongodb.net/?appName=Cluster0`)
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users',usersRouter);
+app.use('/api/pets',petsRouter);
+app.use('/api/adoptions',adoptionsRouter);
+app.use('/api/sessions',sessionsRouter);
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
