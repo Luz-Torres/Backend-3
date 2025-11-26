502680c36365aa5e48ec60da15fe0e34aea7a26b
Luz Torres <luxztorres@gmail.com>
2025-11-25 23:06:13 -0300

    entrega final

--- src/app.js
+++ src/app.js
@@ -8,15 +8,30 @@ import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 8080;
 const CONNECTION = mongoose.connect(`mongodb+srv://lxz:adminx2@cluster0.x8smpie.mongodb.net/?appName=Cluster0`)
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h1> API funcionando correctamente</h1>
+    <p>Rutas disponibles:</p>
+    <ul>
+        <li>/api/users</li>
+        <li>/api/pets</li>
+        <li>/api/adoptions</li>
+        <li>/api/sessions</li>
+        <li>/api/mocks</li>
+    </ul>
+    `);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
